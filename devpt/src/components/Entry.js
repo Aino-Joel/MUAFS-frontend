ea2776cd912bb8015d94f0d248aba2674b30ec5c
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-01 10:24:15 +0300

    the react app

---
+++ devpt/src/components/Entry.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import { Link } from "react-router-dom";
+
+function Entry() {
+
+    return (
+        <div>
+            <Header />
+           hry
+       
+         </div>     
+                
+          
+                 
+    )
+   }
+
+export default Entry
