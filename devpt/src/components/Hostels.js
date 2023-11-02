b814f0409611e412b458f494040f8d873e7a0b7e
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-02 16:24:01 +0300

    modifications

--- devpt/src/components/Hostels.js
+++ devpt/src/components/Hostels.js
@@ -9,7 +9,7 @@ function Hostels() {
     <div>
         <Header />
         <div class = "product">
-                <h2 class = "product-category">Recommended for you</h2>
+                <h2 class = "product-category">Kikoni Hostels</h2>
                 <div class = "product-container">
                     <div class = "product-card">
                         <div class = "product-image">  
@@ -78,15 +78,87 @@ function Hostels() {
             </div>
             < div className="product">
   <h2 className="product-category">
-    Popular Residences &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;
-    &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
-    &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;
-    &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;{" "}
-    <span className='price'>
-
-    <Link to={'/ratings'}>View all hostels</Link>
-      
-    </span>
+    Wandegeya Hostels
+    
+  </h2>
+  <br />
+  <div className="product-container">
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 64000</span>
+        <span className="actual-price">ugshs 85000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="../pictures/man8.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="../pictures/kid9.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+  </div>
+</div>
+< div className="product">
+  <h2 className="product-category">
+    Kikumi Kikumi Hostels
+    
   </h2>
   <br />
   <div className="product-container">
