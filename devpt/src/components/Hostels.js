5a107502ed021ef2436dc4b59242d7c3e620e057
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-02 16:18:00 +0300

    adding hostels

--- devpt/src/components/Hostels.js
+++ devpt/src/components/Hostels.js
@@ -8,7 +8,161 @@ function Hostels() {
   return (
     <div>
         <Header />
-<h1>hostels</h1>
+        <div class = "product">
+                <h2 class = "product-category">Recommended for you</h2>
+                <div class = "product-container">
+                    <div class = "product-card">
+                        <div class = "product-image">  
+                        <img src = "https://i.pinimg.com/564x/b1/04/5f/b1045fdf96ef94318d120bdd917ded2d.jpg" class = "product-thumb" alt = ""/>
+                    </div>
+                    <div class = "product-info">
+                        <br/>
+                    <h1 class = "product-brand"><Link to={'/hostels'}>Nakiyingi Hostel, Kikoni</Link></h1>
+                    <br/>
+                  
+                <span class = "price">4.2(33)</span><span class = "actual-price">shs 1,100,000 </span>
+                </div>
+                </div>
+                <div class = "product-card">
+                    <div class = "product-image">
+                       
+                    <img src = "./pictures/room1.jpg" class = "product-thumb" alt = ""/>
+                </div>
+                <div class = "product-info">
+                    <br/>
+                <h2 class = "product-brand">Soam</h2>
+                <br/>
+                <span class = "price">ugshs 52000</span><span class = "actual-price">ugshs 65000 </span>
+                
+</div>      
+            </div>
+            <div class = "product-card">
+                    <div class = "product-image">
+                       
+                    <img src = "./pictures/room1.jpg" class = "product-thumb" alt = ""/>
+                </div>
+                <div class = "product-info">
+                    <br/>
+                <h2 class = "product-brand">Soam</h2>
+                <br/>
+                <span class = "price">ugshs 52000</span><span class = "actual-price">ugshs 65000 </span>
+                
+</div>      
+            </div>
+            <div class = "product-card">
+                    <div class = "product-image">
+                       
+                    <img src = "./pictures/room1.jpg" class = "product-thumb" alt = ""/>
+                </div>
+                <div class = "product-info">
+                    <br/>
+                <h2 class = "product-brand">Soam</h2>
+                <br/>
+                <span class = "price">ugshs 52000</span><span class = "actual-price">ugshs 65000 </span>
+                
+</div>      
+            </div>
+            <div class = "product-card">
+                    <div class = "product-image">
+                       
+                    <img src = "./pictures/room1.jpg" class = "product-thumb" alt = ""/>
+                </div>
+                <div class = "product-info">
+                    <br/>
+                <h2 class = "product-brand">Soam</h2>
+                <br/>
+                <span class = "price">ugshs 52000</span><span class = "actual-price">ugshs 65000 </span>
+                
+</div>      
+            </div>
+            </div>
+            < div className="product">
+  <h2 className="product-category">
+    Popular Residences &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;
+    &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
+    &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;
+    &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;{" "}
+    <span className='price'>
+
+    <Link to={'/ratings'}>View all hostels</Link>
+      
+    </span>
+  </h2>
+  <br />
+  <div className="product-container">
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 64000</span>
+        <span className="actual-price">ugshs 85000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="./pictures/room1.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="../pictures/man8.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+    <div className="product-card">
+      <div className="product-image">
+        <img src="../pictures/kid9.jpg" className="product-thumb" alt="" />
+      </div>
+      <div className="product-info">
+        <h2 className="product-brand">brand</h2>
+        <span className="price">ugshs 52000</span>
+        <span className="actual-price">ugshs 65000 </span>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
     </div>
   )
 }
