969bcd0905a7ff6b67513838358c7febe75d707f
AinoJoel <ainojoe77@gmail.com>
2023-11-03 21:02:17 +0300

    Removed Delete feature in HostelDetails

--- frontend/src/HostelDetails.js
+++ frontend/src/HostelDetails.js
@@ -1,18 +1,9 @@
-import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const HostelDetails = () => {
     const { id } = useParams();
     const { data:hostel, error, isLoading } = useFetch('http://localhost:8000/hostels/'+id);
     const history = useHistory();
-
-    const handleClick = () => {
-        fetch('http://localhost:8000/hostels/'+ hostel.id, {
-            method: 'DELETE'
-        }).then(() => {
-            history.push('/')
-        })
-    }
 
     return ( 
         <div className="hostel-details">
